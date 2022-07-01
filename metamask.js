4fa7d47724075666b863490a995e569615fec544
tarun kaushik <86646398+ktarun1419@users.noreply.github.com>
2022-07-01 10:52:51 +0530

    Update metamask.js

--- metamask.js
+++ metamask.js
@@ -8,10 +8,6 @@ let amount=document.getElementById('value')*1e18
 function connectWallet(){
     if (window.ethereum) {
     window.ethereum.request({ method: "eth_requestAccounts" });
-    const chainId = await window.ethereum.request({
-      method: "eth_chainId",
-    });
-    console.log(chainId);
     window.web3 = new Web3(window.ethereum);
     return true;
   }
